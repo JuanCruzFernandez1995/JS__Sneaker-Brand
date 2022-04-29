6f6cea5161f8c1b905b4488539d43eb7095c7076
Juan Cruz Fernandez <juancruzfernandez1995@gmail.com>
2022-04-28 23:37:42 -0300

    subiendo repositorio

---
+++ stock.js
@@ -0,0 +1,14 @@
+let stockProductos = [
+    {id: 1, nombre: "Nike Air Jordan", tipo: "buzo", cantidad: 1, precio: 20000, talle: "-", img: './img/airJordan.jpg'},
+    {id: 2, nombre: "Nike Air Jordan", tipo: "buzo", cantidad: 1, precio: 20000, talle: "-", img: './img/airJordan.jpg'},
+    {id: 3, nombre: "Nike Air Jordan", tipo: "buzo", cantidad: 1, precio: 20000, talle: "-", img: './img/airJordan.jpg'},
+    {id: 4, nombre: "Nike Air Jordan", tipo: "buzo", cantidad: 1, precio: 20000, talle: "-", img: './img/airJordan.jpg'},
+    {id: 5, nombre: "Nike Air Force 1", tipo: "buzo", cantidad: 1, precio: 18000, talle: "-", img: './img/airForce.jpg'},
+    {id: 6, nombre: "Nike Air Force 1", tipo: "buzo", cantidad: 1, precio: 18000, talle: "-", img: './img/airForce.jpg'},
+    {id: 7, nombre: "Nike Air Force 1", tipo: "remera", cantidad: 1, precio: 18000, talle: "-", img: './img/airForce.jpg'},
+    {id: 8, nombre: "Nike Air Force 1", tipo: "remera", cantidad: 1, precio: 18000, talle: "-", img: './img/airForce.jpg'},
+    {id: 9, nombre: "Nike Stephen Janosky", tipo: "remera", cantidad: 1, precio: 12000, talle: "-", img: './img/nikeJanosky.jpg'},
+    {id: 10, nombre: "Nike Stephen Janosky", tipo: "remera", cantidad: 1, precio: 12000, talle: "-", img: './img/nikeJanosky.jpg'},
+    {id: 11, nombre: "Nike Stephen Janosky", tipo: "remera", cantidad: 1,precio: 12000, talle: "-", img: './img/nikeJanosky.jpg'},
+    {id: 12, nombre: "Nike Stephen Janosky", tipo: "remera", cantidad: 1, precio: 12000, talle: "-", img: './img/nikeJanosky.jpg'},
+]
